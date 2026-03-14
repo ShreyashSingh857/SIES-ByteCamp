f9f12bf254eb3ff0344bfbe89c373f904b6155c4
Shreyash Singh <singhshreyash857@gmail.com>
2026-03-14 13:26:27 +0530

    Revert "Database"

--- AI-Engine/src/utils/paths.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-function toProjectRelativePath(repositoryPath, absoluteFilePath) {
-  return path.relative(repositoryPath, absoluteFilePath).split(path.sep).join('/');
-}
-
-module.exports = {
-  toProjectRelativePath,
-};
--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -1,28 +1,5 @@
-// Centralized re-exports for store actions and store instance
+// for easier imports
 import {toggleTheme, setTheme} from "./slices/themeSlice.js";
-import {
-  setSelectedNode,
-  clearSelection,
-  setScanStatus,
-  setScanProgress,
-  addRepo,
-  removeRepo,
-  updateRepoStatus,
-  setFilterLangs,
-  setFilterTypes,
-} from "./slices/graphSlice.js";
 
-export {
-  toggleTheme,
-  setTheme,
-  setSelectedNode,
-  clearSelection,
-  setScanStatus,
-  setScanProgress,
-  addRepo,
-  removeRepo,
-  updateRepoStatus,
-  setFilterLangs,
-  setFilterTypes,
-};
+export { toggleTheme, setTheme };
 export { default as store } from "./store.js";
